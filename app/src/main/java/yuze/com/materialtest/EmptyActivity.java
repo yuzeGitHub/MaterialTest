dafede2cd495bacae6a98d59e3c18551ae0cea5d
tangyuze <15510756387@163com>
2017-07-14 13:59:44 +0800

    删除了 EditText 的包

--- app/src/main/java/yuze/com/materialtest/EmptyActivity.java
+++ app/src/main/java/yuze/com/materialtest/EmptyActivity.java
@@ -10,7 +10,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
